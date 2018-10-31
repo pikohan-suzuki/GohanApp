83874f7b549625f1e40c18ec32ed0f405ced1cf7
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-10-31 22:46:15 +0900

    検索ボタンを作成

--- KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MainActivity.java
+++ KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.a81809.kit_map;
 
+import android.app.ActivityOptions;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
+
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        hideSystemUI();
+
     }
+
+//    private void hideSystemUI() {
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//        );
+//    }
 }
