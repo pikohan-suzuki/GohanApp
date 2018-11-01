2ac9ff9542365db186bb0bca3246055089ba5806
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-11-01 19:21:18 +0900

    タッチイベントをview.onTouchに変更

--- KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MainActivity.java
+++ KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -17,72 +18,91 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private GestureDetector mGestureDetector;
-    private ScaleGestureDetector mScaleDetector;
-
-    private ImageView imageView;
+    private ScaleGestureDetector mScaleGestureDetector;
+    private ImageView mImageView;
     private TextView x_range;
     private TextView y_range;
-    private TextView distance;
-    private FrameLayout.LayoutParams layoutParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mImageView = findViewById(R.id.droid_Image);
+        x_range = findViewById(R.id.x_range);
+        y_range = findViewById(R.id.y_range);
+
+        /* Touch event */
+        mImageView.setOnTouchListener(mTouchEventLister);
+        mScaleGestureDetector = new ScaleGestureDetector(this, mScaleGestureListener);
         mGestureDetector = new GestureDetector(this, mGestureListener);
-        mScaleDetector = new ScaleGestureDetector(this, scaleGestureListener);
-
-//        imageView = findViewById(R.id.map_image);
-        x_range = findViewById(R.id.range_x);
-        y_range=findViewById(R.id.range_y);
-        distance=findViewById(R.id.distance);
-
     }
 
 
-
-    //モーションイベントを検知するリスナー
-    private GestureDetector.SimpleOnGestureListener mGestureListener = new GestureDetector.SimpleOnGestureListener() {
+    private View.OnTouchListener mTouchEventLister = new View.OnTouchListener() {
         @Override
-        public boolean onDoubleTap(MotionEvent e) {
-            // ダブルタップ時の処理.
+        public boolean onTouch(View v, MotionEvent e) {
+            mScaleGestureDetector.onTouchEvent(e);
+            mGestureDetector.onTouchEvent(e);
+            Log.d("debug", "onTouch");
             return true;
         }
-
-
     };
 
-    //スケーリング変換を検知するリスナー
-    private ScaleGestureDetector.SimpleOnScaleGestureListener scaleGestureListener = new ScaleGestureDetector.SimpleOnScaleGestureListener() {
 
-        private PointF mLastPoint = new PointF();
+    private ScaleGestureDetector.OnScaleGestureListener mScaleGestureListener
+            = new ScaleGestureDetector.OnScaleGestureListener() {
 
         @Override
         public boolean onScaleBegin(ScaleGestureDetector detector) {
-            mLastPoint.set(detector.getCurrentSpanX() / 2.f, detector.getCurrentSpanY() / 2.f);
+            Log.d("debug", "on Scale Begin");
             return true;
         }
 
-        @Override
         public boolean onScale(ScaleGestureDetector detector) {
-            float factor = detector.getScaleFactor();
-            float x = detector.getCurrentSpanX() / 2.f;
-            float y = detector.getCurrentSpanY() / 2.f;
-            boolean useFitScale = false;
-
-            float diffX = x - mLastPoint.x;
-            float diffY = y - mLastPoint.y;
-
-            x_range.setText(String.valueOf(diffX));
-            y_range.setText(String.valueOf(diffY));
-            mLastPoint.set(x, y);
-
+            Log.d("debug", "on Scale");
             return true;
         }
 
-        @Override
         public void onScaleEnd(ScaleGestureDetector detector) {
+            Log.d("debug", "on Scale End.");
         }
     };
+
+    private GestureDetector.OnGestureListener mGestureListener =
+            new GestureDetector.OnGestureListener() {
+                @Override
+                public boolean onDown(MotionEvent motionEvent) {
+                    Log.d("debug", "onDown");
+                    return false;
+                }
+
+                @Override
+                public void onShowPress(MotionEvent motionEvent) {
+                    Log.d("debug", "onShowPress");
+                }
+
+                @Override
+                public boolean onSingleTapUp(MotionEvent motionEvent) {
+                    Log.d("debug", "onSingleTapUp");
+                    return false;
+                }
+
+                @Override
+                public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                    Log.d("debug", "onScroll");
+                    return false;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent motionEvent) {
+                    Log.d("debug", "onLongPress");
+                }
+
+                @Override
+                public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                    Log.d("debug", "onFling");
+                    return false;
+                }
+            };
 }
