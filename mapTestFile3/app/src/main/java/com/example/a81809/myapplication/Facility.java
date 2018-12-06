663c3da52d0eb6d11bc8ba0406938773b192d553
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-12-07 00:37:13 +0900

    施設情報をフィールドに保存する処理を追加

--- mapTestFile3/app/src/main/java/com/example/a81809/myapplication/Facility.java
+++ mapTestFile3/app/src/main/java/com/example/a81809/myapplication/Facility.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class Facility {
     private ImageView image;
     private Context context;
@@ -14,6 +16,15 @@ public class Facility {
         this.context=context;
         this.image = new ImageView(this.context);
         this.type = database.getFacilityType(building_number,floor,id);
-        
+        int[] range = database.getFacilityRange(building_number,floor,id);
+        this.x= range[0];
+        this.y=range[1];
+        if(type==0)
+            image.setImageResource(R.drawable.printer_icon);
+        else
+            image.setImageResource(R.drawable.vending_machine_icon);
+    }
+    private void setImage(){
+
     }
 }
