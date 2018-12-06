11071f63aeed8d02985ee6aa4768d476b880a727
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-12-06 11:07:27 +0900

    データベース検索用の処理を追加

--- mapTestFile3/app/src/main/java/com/example/a81809/myapplication/MainActivity.java
+++ mapTestFile3/app/src/main/java/com/example/a81809/myapplication/MainActivity.java
@@ -18,47 +18,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            InputStream fp = getApplication().getAssets().open("asdf.txt");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        DatabaseOpenHelper helper = new DatabaseOpenHelper(getApplicationContext(),"test.db", null, 1);
-        SQLiteDatabase db = helper.getReadableDatabase();
-        Toast toast = Toast.makeText(this,searchData(db),Toast.LENGTH_SHORT);
-    }
-
-    public String searchData(SQLiteDatabase db) {
-        // Cursorを確実にcloseするために、try{}～finally{}にする
-        Cursor cursor = null;
-        try {
-            //SQL文
-            String sql = "SELECT * FROM test";
-
-            //SQL文の実行
-//            cursor = db.query("test", new String[]{"id", "name"}, null, null, null, null, null, null);
-            cursor = db.rawQuery(sql,new String[]{});
-            // 検索結果をcursorから読み込んで返す
-            return readCursor(cursor);
-        } catch(Exception e) {
-            e.printStackTrace();
-            return null;
-        } finally {
-            // Cursorを忘れずにcloseする
-            if (cursor != null) {
-                cursor.close();
-            }
-        }
-    }
-    private String readCursor(Cursor cursor ){
-        //カーソル開始位置を先頭にする
-        cursor.moveToFirst();
-        String str="";
-        for (int i = 1; i <= cursor.getCount(); i++) {
-            //SQL文の結果から、必要な値を取り出す
-            str += cursor.getString(0);
-            cursor.moveToNext();
-        }
-        return str;
+        DatabaseRead database = new DatabaseRead(getApplication(),"database.db");
+        String str = database.getBuildingName();
+        Log.d("debug","pppppppppp"+str);
     }
 }
