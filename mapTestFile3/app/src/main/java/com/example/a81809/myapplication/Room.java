83fce04612c9aca13a48fbb591013f0debd5b2be
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-12-08 01:32:19 +0900

    フィールドとコンストラクタを追加

--- mapTestFile3/app/src/main/java/com/example/a81809/myapplication/Room.java
+++ mapTestFile3/app/src/main/java/com/example/a81809/myapplication/Room.java
@@ -1,9 +1,24 @@
 package com.example.a81809.myapplication;
 
+import android.content.Context;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
 public class Room {
-
-    public Room(){
-
+    private TextView roomTextView;
+    private Context context;
+    private float x;
+    private float y;
+    private int room_num;
+    private String name;
+    public Room(Context context, FrameLayout layout, DatabaseRead database, int building_number, int floor, int room_num){
+        this.context=context;
+        this.room_num=room_num;
+        String[] info = database.getRoomInfo(building_number,floor,room_num);
+        this.name=info[0];
+        this.x= Float.parseFloat(info[1]);
+        this.y=Float.parseFloat(info[2]);
+        roomTextView=new TextView(context);
     }
     public void onScroll(float moveX,float moveY){
 
