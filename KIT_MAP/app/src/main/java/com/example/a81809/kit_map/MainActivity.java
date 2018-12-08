4b7869c5db722795d7d2e3f686fec9329dbb440c
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-12-08 15:24:33 +0900

    Actionbarをshow,hideする処理を追加

--- KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MainActivity.java
+++ KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.a81809.kit_map;
 
+import android.content.Intent;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
-    public static Image image;
+    private Image image;
     private Room[] rooms;
     private Facility[] faclities;
     private FrameLayout parent_layout;
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     public static int building_number;
     public static int floor;
     private Point focusRange;
+    private boolean touchFlg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         parent_layout = findViewById(R.id.parent_layout);
         building_number = 0;
         floor = 0;
+        touchFlg = true;
 
         //タッチイベント
         parent_layout.setOnTouchListener(mTouchEventListener);
@@ -51,18 +57,38 @@ public class MainActivity extends AppCompatActivity {
             int[] room_numbers = database.getRoomNumbers(23, 1);
             rooms = new Room[room_numbers.length];
             for (int i = 0; i < room_numbers.length; i++)
-                rooms[i] = new Room(getApplication(), parent_layout, database, 23, 1, room_numbers[i]);
-            int[] facility_numbers = database.getFacilityNumber(23,1);
-            faclities=new Facility[facility_numbers.length];
-            for(int i=0;i<facility_numbers.length;i++)
-                faclities[i]=new Facility(getApplication(),parent_layout,database,23,1,facility_numbers[i]);
-            for (int i = 0; i < room_numbers.length; i++)
-                rooms[i].changeRange();
-            for(int i=0;i<facility_numbers.length;i++)
-                faclities[i].changeRange();
+                rooms[i] = new Room(getApplication(), parent_layout, database, 23, 1,
+                        room_numbers[i], image.getImageSize(), image.getImageLocation());
+            int[] facility_numbers = database.getFacilityNumber(23, 1);
+            faclities = new Facility[facility_numbers.length];
+            for (int i = 0; i < facility_numbers.length; i++)
+                faclities[i] = new Facility(getApplication(), parent_layout, database, 23, 1,
+                        facility_numbers[i], image.getImageSize(), image.getImageLocation());
         }
+    }
 
-//        image.scale();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.b23_1:
+
+                return true;
+            case R.id.b23_2:
+
+                return true;
+            case R.id.b23_3:
+
+                return true;
+        }
+        return false;
     }
 
     private Point getViewSize(View v) {
@@ -74,8 +100,8 @@ public class MainActivity extends AppCompatActivity {
     private View.OnTouchListener mTouchEventListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
-                mScaleGestureDetector.onTouchEvent(motionEvent);
-                mGestureDetector.onTouchEvent(motionEvent);
+            mScaleGestureDetector.onTouchEvent(motionEvent);
+            mGestureDetector.onTouchEvent(motionEvent);
             return true;
 
         }
@@ -85,11 +111,14 @@ public class MainActivity extends AppCompatActivity {
             new GestureDetector.OnGestureListener() {
                 @Override
                 public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float distanceX, float distanceY) {
+                    touchFlg = false;
+                    getSupportActionBar().hide();
+
                     image.onScroll(distanceX, distanceY);
-                    for(int i=0;i<faclities.length;i++)
-                        faclities[i].onScroll(distanceX,distanceY);
+                    for (int i = 0; i < faclities.length; i++)
+                        faclities[i].onScroll(image.getImageSize(), image.getImageLocation(), distanceX, distanceY);
                     for (int i = 0; i < rooms.length; i++)
-                        rooms[i].onScroll(distanceX,distanceY);
+                        rooms[i].onScroll(image.getImageSize(), image.getImageLocation(), distanceX, distanceY);
                     return false;
                 }
 
@@ -105,6 +134,13 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public boolean onSingleTapUp(MotionEvent motionEvent) {
+                    if (touchFlg) {
+                        touchFlg = false;
+                        getSupportActionBar().hide();
+                    } else {
+                        touchFlg = true;
+                        getSupportActionBar().show();
+                    }
                     return false;
                 }
 
@@ -123,10 +159,17 @@ public class MainActivity extends AppCompatActivity {
             new ScaleGestureDetector.OnScaleGestureListener() {
                 @Override
                 public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+                    touchFlg = false;
+                    getSupportActionBar().hide();
+
                     float factor = scaleGestureDetector.getScaleFactor();
                     if (focusRange.x == 0 && focusRange.y == 0)
                         focusRange.set((int) scaleGestureDetector.getFocusX(), (int) scaleGestureDetector.getFocusY());
                     image.onScale(focusRange.x, focusRange.y, factor);
+                    for (int i = 0; i < faclities.length; i++)
+                        faclities[i].onScale(image.getImageSize(), image.getImageLocation());
+                    for (int i = 0; i < rooms.length; i++)
+                        rooms[i].onScale(image.getImageSize(), image.getImageLocation());
                     return false;
                 }
 
@@ -140,4 +183,8 @@ public class MainActivity extends AppCompatActivity {
                 public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
                 }
             };
+
+    private void hideActionBar(){
+        
+    }
 }
