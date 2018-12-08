b60f1bb169cb943e6a96f4460bf99e4a0e7710a5
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-12-08 11:22:56 +0900

    onScrollメソッドを追加

--- KIT_MAP/app/src/main/java/com/example/a81809/kit_map/Facility.java
+++ KIT_MAP/app/src/main/java/com/example/a81809/kit_map/Facility.java
@@ -1,6 +1,7 @@
 package com.example.a81809.kit_map;
 
 import android.content.Context;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -10,25 +11,44 @@ public class Facility {
     private ImageView image;
     private Context context;
     private int type;
-    private int x;
-    private int y;
+    private float x;
+    private float y;
+    private int width;
+    private int height;
     public  Facility(Context context,FrameLayout layout ,DatabaseRead database, int building_number, int floor, int id){
         this.context=context;
         this.image = new ImageView(this.context);
         this.type = database.getFacilityType(building_number,floor,id);
-        int[] range = database.getFacilityRange(building_number,floor,id);
+        float[] range = database.getFacilityRange(building_number,floor,id);
         this.x= range[0];
         this.y=range[1];
         if(type==0)
             image.setImageResource(R.drawable.printer_icon);
         else
             image.setImageResource(R.drawable.vending_machine_icon);
+        layout.addView(this.image);
+        this.width=144;
+        this.height=144;
+        image.setLayoutParams(new FrameLayout.LayoutParams(this.width,this.height));
+    }
+    public void changeRange(){
+        if(width==0&&height==0){
+            this.width=image.getWidth();
+            this.height=image.getHeight();
+        }
+        this.x= MainActivity.image.getImageLocation().x + MainActivity.image.getImageRange().x * this.x-this.width/2;
+        this.y=MainActivity.image.getImageLocation().y + MainActivity.image.getImageRange().y * this.y-this.height/2;
+        image.setX(this.x);
+        image.setY(this.y);
     }
     private void setImage(){
 
     }
 
     public void onScroll(float moveX,float moveY){
-
+        this.x = this.x - moveX;
+        this.y = this.y - moveY;
+        image.setX(this.x);
+        image.setY(this.y);
     }
 }
--- KIT_MAP/app/src/main/java/com/example/a81809/kit_map/Room.java
+++ KIT_MAP/app/src/main/java/com/example/a81809/kit_map/Room.java
@@ -1,6 +1,7 @@
 package com.example.a81809.kit_map;
 
 import android.content.Context;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -10,17 +11,47 @@ public class Room {
     private float x;
     private float y;
     private int room_num;
+    private int textSize;
+    private int width;
+    private int height;
     private String name;
-    public Room(Context context, FrameLayout layout, DatabaseRead database, int building_number, int floor, int room_num){
-        this.context=context;
-        this.room_num=room_num;
-        String[] info = database.getRoomInfo(building_number,floor,room_num);
-        this.name=info[0];
-        this.x= Float.parseFloat(info[1]);
-        this.y=Float.parseFloat(info[2]);
-        roomTextView=new TextView(context);
-    }
-    public void onScroll(float moveX,float moveY){
 
+    public Room(Context context, FrameLayout layout, DatabaseRead database, int building_number, int floor, int room_num) {
+        this.context = context;
+        this.room_num = room_num;
+        String[] info = database.getRoomInfo(building_number, floor, room_num);
+        this.name = info[0];
+        this.x = Float.parseFloat(info[1]);
+        this.y = Float.parseFloat(info[2]);
+        roomTextView = new TextView(context);
+        layout.addView(roomTextView);
+        String str[] = name.split(",");
+        if (str.length == 1)
+            roomTextView.setText(building_number + "-" + room_num + "\n" + str[0]);
+        else
+            roomTextView.setText(building_number + "-" + room_num + "\n" + str[0] + "\n" + str[1]);
+        roomTextView.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        roomTextView.setBackgroundResource(R.drawable.marukado);
+        roomTextView.setGravity(1);
+
+
+    }
+
+    public void changeRange() {
+        if (width == 0 && height == 0) {
+            this.width = roomTextView.getWidth();
+            this.height = roomTextView.getHeight();
+        }
+        this.x = MainActivity.image.getImageLocation().x + MainActivity.image.getImageRange().x * this.x - this.width / 2;
+        this.y = MainActivity.image.getImageLocation().y + MainActivity.image.getImageRange().y * this.y - this.height / 2;
+        roomTextView.setX(this.x);
+        roomTextView.setY(this.y);
+    }
+
+    public void onScroll(float moveX, float moveY) {
+        this.x = this.x - moveX;
+        this.y = this.y - moveY;
+        roomTextView.setX(this.x);
+        roomTextView.setY(this.y);
     }
 }
