21e780fec0bfcf518b3c117f9b9037de0c9711d4
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2018-12-16 13:33:55 +0900

    階層ごとに現在地が切り替わるように変更

--- KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MyLocation.java
+++ KIT_MAP/app/src/main/java/com/example/a81809/kit_map/MyLocation.java
@@ -1,21 +1,26 @@
 package com.example.a81809.kit_map;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.location.Location;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 public class MyLocation {
     private ImageView image;
     private Context context;
+    private static final int imageSize= 50;
     public MyLocation(Context context){
         this.context =context;
         image=new ImageView(this.context);
         image.setImageResource(R.drawable.location);
-        image.setLayoutParams(new FrameLayout.LayoutParams(50,50));
+        image.setLayoutParams(new FrameLayout.LayoutParams(imageSize,imageSize));
     }
-    public void setLocationIcon(FrameLayout layout,boolean locationShowing){
+    public void setLocationIcon(FrameLayout layout, boolean locationShowing, Location location,Point mapLocation,Point mapSize,
+                                double top_latitude,double left_longitude,double latitude,double longitude){
         if(!locationShowing) layout.addView(image);
-
+            image.setX((float)(mapLocation.x+(location.getLongitude()-left_longitude)*mapSize.x/longitude-imageSize/2));
+            image.setY((float)(mapLocation.y+(top_latitude-location.getLatitude())*mapSize.y/latitude-imageSize/2));
     }
     public void removeLocationIcon(FrameLayout layout){
         layout.removeView(image);
